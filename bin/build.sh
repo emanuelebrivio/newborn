71fd44e7259ca96d1b71354d77bd2bc30e3076dd
Alessio Zappa <alessio@plasticpanda.com>
2015-04-01 09:05:16 +0200

    verbose nodemon log

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,4 @@
 
 jade $(pwd)/*.jade
 stylus -c $(pwd)/static/css/style.styl
-uglifyjs $(pwd)/static/js/main.js --output $(pwd)/static/js/main.min.js
+uglifyjs $(pwd)/static/js/main.js -o $(pwd)/static/js/main.min.js
